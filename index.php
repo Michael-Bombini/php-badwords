4b4c105ef6ec00ae473871a971a3dbb4c744282a
Michael Bombini <bombini.michael@gmail.com>
2022-07-07 14:20:17 +0200

    Completed exercise

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+*/
+
+$testo = "Lorem ipsum, 
+dolor sit ciao testo lorem prova LorEM LOREM lorem LoreM";
+
+
+$daCensurare = $_GET["censura"];
+$testoCensurato = "";
+$testoCensurato = str_ireplace($daCensurare,"***",$testo);
+echo "Testo : " . "  " . $testo;
+echo "<br>";
+echo "Lunghezza testo : " . strlen($testo);
+echo "<br>";
+echo "Testo da censurare:" . "  " . $daCensurare;
+echo "<br>";
+echo "Testo censurato " . " " . $testoCensurato;
+?>
